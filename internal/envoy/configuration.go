49de14c7c93f5deb4c79c1aa2fa5c4cd4d32f0a2
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2022-12-26 18:13:19 -0300

    Fix resource hash for envoy configuration

--- internal/envoy/configuration.go
+++ internal/envoy/configuration.go
@@ -3,12 +3,12 @@ package envoy
 import (
 	"bytes"
 	"crypto/sha256"
-	"encoding/gob"
 	"strconv"
 	"sync/atomic"
 
 	"github.com/envoyproxy/go-control-plane/pkg/cache/types"
 	"github.com/envoyproxy/go-control-plane/pkg/cache/v3"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 type GenericConfiguration struct {
@@ -30,11 +30,19 @@ func (g *GenericConfiguration) HashResources() []byte {
 
 	hash := sha256.New()
 
-	enc := gob.NewEncoder(hash)
+	for _, resources := range g.Resources {
 
-	err := enc.Encode(g.Resources)
-	if err != nil {
-		panic(err)
+		for _, resource := range resources {
+
+			msg, err := protojson.Marshal(resource)
+			if err != nil {
+				panic(err)
+			}
+
+			hash.Sum(msg)
+
+		}
+
 	}
 
 	return hash.Sum(nil)
