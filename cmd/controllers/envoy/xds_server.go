84467591c9f68d0c779e2f49af5272f3fad75e0f
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2022-12-26 14:49:38 -0300

    Add aws configuration

--- cmd/controllers/envoy/xds_server.go
+++ cmd/controllers/envoy/xds_server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/spf13/cobra"
 	awsctlr "github.com/upper-institute/ops-control/internal/aws"
 	envoyctlr "github.com/upper-institute/ops-control/internal/envoy"
@@ -31,16 +32,24 @@ var (
 					genericConfiguration = &envoyctlr.GenericConfiguration{}
 				)
 
-				ctx := context.Background()
-
 				for {
+
+					ctx := context.Background()
 
 					if enableAwsEnvoyFrontProxy {
 
 						if awsFrontProxy == nil {
+
+							config, err := config.LoadDefaultConfig(ctx)
+							if err != nil {
+								log.Fatalln(err.Error())
+							}
+
 							awsFrontProxy = &awsctlr.FrontProxy{
+								Config:               config,
 								GenericConfiguration: genericConfiguration,
 							}
+
 						}
 
 						err := awsFrontProxy.LoadConfigurationFromCloudMap(ctx)
